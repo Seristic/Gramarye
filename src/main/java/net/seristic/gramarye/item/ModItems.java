1197874d73dc15b1ece9fee8f576e70dfb9bcb6f
Seristic <StasisGamingYT@Gmail.com>
2024-03-19 20:29:22 +0000

    Added a new custom effect called CursedItem which will cause the player to take damage over the time that they have the fragments which will deal more damage at a faster rate, the more they have. ModItems has been adjusted to use it.

--- src/main/java/net/seristic/gramarye/item/ModItems.java
+++ src/main/java/net/seristic/gramarye/item/ModItems.java
@@ -7,6 +7,7 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 import net.seristic.gramarye.Gramarye;
+import net.seristic.gramarye.item.custom.CursedItem;
 
 public class ModItems extends Item {
     public static final DeferredRegister<Item> ITEMS =
@@ -16,7 +17,7 @@ public class ModItems extends Item {
             () -> new Item(new Item.Properties().stacksTo(1)
             ));
     public static final RegistryObject<Item> FORBIDDEN_FRAGMENT = ITEMS.register("forbidden_fragment",
-            () -> new Item(new Item.Properties().stacksTo(1)
+            () -> new CursedItem(new Item.Properties().stacksTo(1)
                     .rarity(Rarity.EPIC)
             ));
 
